9cedc59f93c4e0db7159235aab097c30088853cd
jorgemaciaspina <jorgekbw3@gmail.com>
2025-03-07 08:56:57 -0600

    Initial setup and fixed compatibility with tailwindcss v4

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    fontFamily: {
+      satoshi: ['Satoshi', 'sans-serif'],
+      inter: ['Inter', 'sans-serif'],
+    },
+    colors: {
+      'primary-orange': '#FF5722',
+    },
+    extend: {
+      fontFamily: {
+        satoshi: ['Satoshi', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        'primary-orange': '#FF5722',
+      }
+    },
+  },
+  plugins: [],
+}
