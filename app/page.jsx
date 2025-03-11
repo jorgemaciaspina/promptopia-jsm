d2f3aca959631d0d13e47058eccd38065d2c0b47
jorgemaciaspina <jorgekbw3@gmail.com>
2025-03-11 08:51:03 -0600

    Implement a basic welcome information section

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,17 @@
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Promptopia es una herramienta de prompts de IA de código abierto para el mundo moderno para descubrir, crear y compartir prompts creativos
+      </p>
+    </section>
+  );
 };
 
 export default Home;
