e315535760028131717e980e945b923c0f563b46
jorgemaciaspina <jorgekbw3@gmail.com>
2025-03-12 08:44:28 -0600

    Implement basic desktop navigation

--- app/page.jsx
+++ app/page.jsx
@@ -3,9 +3,9 @@ const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
       <h1 className="head_text text-center">
-        Discover & Share
+        Descubre & Comparte
         <br className="max-md:hidden" />
-        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+        <span className="orange_gradient text-center">Prompts Potenciadod por AI </span>
       </h1>
       <p className="desc text-center">
         Promptopia es una herramienta de prompts de IA de código abierto para el mundo moderno para descubrir, crear y compartir prompts creativos
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,6 +6,16 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
+  const isUserLoggedIn = false;
+  const [providers, setProviders] = useState(null);
+
+  useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders();
+      setProviders(response);
+    }
+    setProviders();
+  }, [])
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
@@ -18,10 +28,33 @@ const Nav = () => {
         />
         <p className="logo_text">Promptopia</p>
       </Link>
-      <div className="flex gap-4">
-        <Link href="/create-prompt" className="black_btn">
-          Crear Prompt
-        </Link>
+      {/* Desktop Navigation */}
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Crear Prompt
+            </Link>
+
+            <button type="button" onClick={signOut} className="outline_btn">
+              Salir
+            </button>
+
+            <Link href="/profile">
+              <Image src="/assets/images/logo.svg"
+                width={37}
+                height={37}
+                className="rounded-full"
+                alt="profile"
+              />
+            </Link>
+          </div>
+        ) : (<>
+          {providers && Object.values(providers).map((provider) => (
+            <button onClick={() => signIn(provider.id)} key={provider.name} type="button" className="black_btn">Ingresar</button>
+          ))
+          }
+        </>)}
       </div>
     </nav>
   );
