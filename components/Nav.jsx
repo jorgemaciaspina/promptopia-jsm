e1782b9b50cf44bec61c3e90ed2616c155697579
jorgemaciaspina <jorgekbw3@gmail.com>
2025-03-13 20:38:04 -0600

    Implement sign in with google and saving to mongo

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,16 +6,17 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
+  const isUserLoggedIn = session?.user;
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders();
       setProviders(response);
     }
-    // setProviders();
+    setUpProviders();
   }, [])
   return (
     <nav className="flex-between w-full mb-16 pt-3">
@@ -42,7 +43,7 @@ const Nav = () => {
             </button>
 
             <Link href="/profile">
-              <Image src="/assets/images/logo.svg"
+              <Image src={session?.user.image}
                 width={37}
                 height={37}
                 className="rounded-full"
@@ -62,7 +63,7 @@ const Nav = () => {
         {isUserLoggedIn ? (
           <div className="flex">
             <Image
-              src="/assets/images/logo.svg"
+              src={session?.user.image}
               alt="Promptopia Logo"
               width={30}
               height={30}
