3791645bb4fa2c77526b088640fa69ab0870134c
jorgemaciaspina <jorgekbw3@gmail.com>
2025-03-15 10:03:23 -0600

    Partial implementation of profile page to show info

---
+++ app/profile/page.jsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { useSession } from "next-auth/react";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+import Profile from "@components/Profile";
+
+const MyProfile = () => {
+
+  const { data: session } = useSession();
+  const router = useRouter();
+  const [posts, setPosts] = useState([]);
+
+  const fetchPosts = async () => {
+    const response = await fetch(`/api/users/${session?.user.id}/prompts`);
+    const data = await response.json();
+    setPosts(data);
+  }
+  
+  const handleEdit = async () => {
+    
+  }
+
+  const handleDelete = async () => {
+    
+  }
+
+  useEffect(() => {
+    if (session?.user.id) fetchPosts();
+  }, [session?.user.id])
+
+  return (
+    <Profile
+      name="Mi"
+      desc="Bienvenido a tu perfil"
+      data={posts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  )
+};
+
+export default MyProfile;
